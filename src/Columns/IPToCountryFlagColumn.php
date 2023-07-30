f16d40c3d9a4eafcf47dc2004f805ed1431e9dd0
Mohammad <mohammadhprp@gmail.com>
2023-07-30 14:14:41 +0330

    feat: Add customization option to the column

--- src/Columns/IPToCountryFlagColumn.php
+++ src/Columns/IPToCountryFlagColumn.php
@@ -9,55 +9,161 @@ class IPToCountryFlagColumn extends TextColumn
 {
     protected string $view = 'filament-ip-to-country-flag-column::columns.ip-to-country-flag-column';
 
+    protected ?string $ip = null;
+    protected ?string $flag = null;
+
     protected ?string $countryName = null;
     protected ?string $city = null;
 
-    public function getIpWithFlag(): string
+    protected bool $isIPHide = false;
+    protected bool $isFlagHide = false;
+    protected bool $isLocationHide = false;
+    protected bool $isCountryHide = false;
+    protected bool $isCityHide = false;
+
+    protected string $flagPosition = 'right';
+    protected string $locationPosition = 'below';
+    protected string $locationSeparator = ',';
+
+
+    protected function setUp(): void
     {
-        $ip = $this->getStateFromRecord();
+        parent::setUp();
+    }
+
+
+    public function hideIP(): static
+    {
+        $this->isIPHide = true;
+
+        return $this;
+    }
+
+    public function hideFlag(): static
+    {
+        $this->isFlagHide = true;
+
+        return $this;
+    }
+
+    public function hideLocation(): static
+    {
+        $this->isLocationHide = true;
+
+        return $this;
+    }
+
+    public function hideCountry(): static
+    {
+        $this->isCountryHide = true;
+
+        return $this;
+    }
+
+    public function hideCity(): static
+    {
+        $this->isCityHide = true;
+
+        return $this;
+    }
+
+
+    public function location(string $position = 'below', string $separator = ','): static
+    {
+        $this->locationPosition = $position;
+        $this->locationSeparator = $separator;
+
+        return $this;
+    }
+
+    public function flagPosition(?string $position = 'right'): static
+    {
+        $this->flagPosition = $position;
+
+        return $this;
+    }
+
+
+    public function getIP(): string
+    {
+        $this->ip = $this->getStateFromRecord();
 
         /// Return default state if IP was null
-        if ($ip === null) {
+        if ($this->ip === null) {
+            $this->city = null;
+            $this->countryName = null;
+            $this->flag = null;
+
             return $this->getDefaultState() ?? '-';
         }
 
         /// Check to IP address be valid
-        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+        if (!filter_var($this->ip, FILTER_VALIDATE_IP)) {
             return 'Invalid IP address';
         }
 
         /// Check to IP address not be localhost
-        if ($ip === '127.0.0.1') {
-            return "$ip 🏠";
+        if ($this->ip === '127.0.0.1') {
+            return "$this->ip 🏠";
         }
 
-        $location = $this->ip2Location($ip);
+        $location = $this->ip2Location($this->ip);
 
         $countryCode = $location->get('country_code');
 
         if ($countryCode === null) {
-            return $ip;
+            return $this->ip;
         }
 
         $this->city = $location->get('city');
         $this->countryName = $location->get('country_name');
 
-        $countryFlag = $this->getCountyFlag($countryCode);
+        $this->flag = $this->getCountyFlag($countryCode);
 
-        return "$ip $countryFlag";
+        return $this->ip;
     }
 
-    public function getIpInfo(): string
+    public function getFlag(): ?string
     {
-        if ($this->countryName === null) {
+        return $this->flag;
+    }
+
+    public function getFlagPosition(): string
+    {
+        return $this->flagPosition;
+    }
+
+    public function getLocation(): string
+    {
+        if ($this->isCountryHide || $this->countryName === null) {
             return "$this->city";
         }
 
-        if ($this->city === null) {
+        if ($this->isCityHide || $this->city === null) {
             return "$this->countryName";
         }
 
-        return "$this->city, $this->countryName";
+        return $this->city . $this->locationSeparator . $this->countryName;
+    }
+
+    public function getLocationPosition(): string
+    {
+        return $this->locationPosition;
+    }
+
+    public function getHideIP(): bool
+    {
+        return $this->isIPHide;
+    }
+
+    public function getHideFlag(): bool
+    {
+        return $this->isFlagHide;
+    }
+
+    public function getHideLocation(): bool
+    {
+        return $this->isLocationHide;
     }
 
     private function getCountyFlag(string $countryCode): string
